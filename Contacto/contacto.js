929e67a8181c0de058e09d2129ecb7904a275506
CarlosMiguelSanchezLoreto <sa_carlos@javeriana.edu.co>
2022-02-14 19:33:09 -0500

    Upload project

---
+++ Contacto/contacto.js
@@ -0,0 +1,15 @@
+function Mensaje(){
+
+    var name = document.getElementById("exampleInputName").value;
+    var lastname = document.getElementById("exampleInputApellidos").value;
+    var user = document.getElementById("exampleInputUsuario").value;
+    var email = document.getElementById("exampleInputEmail").value;
+    var tel = document.getElementById("exampleInputTelefono").value;
+    var msg = document.getElementById("exampleInputMensaje").value;
+    var not = document.getElementById("exampleCheck1").value;
+
+
+    var alerta = "Nombre: " + name + " " + lastname + "\n" + "Usuario: " + user + "\n" + "email: " + email + "\n" + "Telefono: " + tel + "\n" + "Mensaje: " + msg + "\n";
+    alert(alerta);
+
+}
